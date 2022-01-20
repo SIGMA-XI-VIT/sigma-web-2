2e632188d2d1bf48c57057c6e2d6f7855955397e
shreyaaass <shreyas.10.singh@gmail.com>
2022-01-21 02:17:24 +0530

    initial commit

---
+++ scroll.js
@@ -0,0 +1,15 @@
+window.sr = ScrollReveal();
+sr.reveal(".blog", {
+  duration: 2000,
+  interval: 200,
+  origin: "top",
+  distance: "200px",
+  reset: true,
+});
+sr.reveal(".buttons-reveal", {
+  duration: 1000,
+  interval: 100,
+  origin: "top",
+  distance: "100px",
+  reset: true,
+});
