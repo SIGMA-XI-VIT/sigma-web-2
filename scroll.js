8a1505cf54bdee8d34657310e7bccc1144f56dfa
shreyaaass <shreyas.10.singh@gmail.com>
2022-01-21 12:04:50 +0530

    abc

--- scroll.js
+++ scroll.js
@@ -3,12 +3,19 @@ sr.reveal(".blog", {
   duration: 2000,
   interval: 200,
   origin: "top",
-  distance: "200px",
+  distance: "150px",
   reset: true,
 });
 sr.reveal(".buttons-reveal", {
   duration: 1000,
   interval: 100,
+  origin: "top",
+  distance: "100px",
+  reset: true,
+});
+sr.reveal(".events-carousel", {
+  duration: 1000,
+  interval: 50,
   origin: "top",
   distance: "100px",
   reset: true,
